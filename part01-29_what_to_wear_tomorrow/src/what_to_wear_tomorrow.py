14f029f5a39336532e4de0c9602e68ae53d6b7f1
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-15 00:17:07 +0800

    Exercise Completed

--- part01-29_what_to_wear_tomorrow/src/what_to_wear_tomorrow.py
+++ part01-29_what_to_wear_tomorrow/src/what_to_wear_tomorrow.py
@@ -1,1 +1,16 @@
 # Write your solution here
+print('What is the weather forecast for tomorrow?')
+temperature = int(input('Temperature:'))
+rain_status = input('Will it rain (yes/no):')
+
+message = 'Wear jeans and a T-shirt \n'
+if temperature <= 20:
+    message += 'I recommend a jumper as well \n'
+if temperature <= 10:
+    message += 'Take a jacket with you \n'
+if temperature <= 5:
+    message += 'Make it a warm coat, actually \nI think gloves are in order \n'
+if rain_status == 'yes':
+    message += "Don't forget your umbrella!"
+    
+print(message)
