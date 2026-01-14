d76f9fee72af55851cfb959f76caeca0526d5415
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-14 23:37:26 +0800

    Exercise Completed

--- part01-26_temperatures/src/temperatures.py
+++ part01-26_temperatures/src/temperatures.py
@@ -1,1 +1,10 @@
 # Write your solution here
+temperature = int(input('Please tyoe in a temperature (F):'))
+
+celsius = (temperature - 32) / 1.8
+
+if celsius < 0:
+    print(f'{temperature} degrees Fahrenheit equals {celsius} degrees Celsius')
+    print("Brr! It's cold in here!")
+else:
+    print(f'{temperature} degrees Fahrenheit equals {celsius} degrees Celsius')
