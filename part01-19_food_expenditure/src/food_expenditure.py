cbd093822180380382a02d44eea0b63fc7bae0df
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-14 23:01:09 +0800

    Exercise Completed

--- part01-19_food_expenditure/src/food_expenditure.py
+++ part01-19_food_expenditure/src/food_expenditure.py
@@ -1,1 +1,11 @@
 # Write your solution here
+eat_lunch = int(input('How many times a week do you eat at the studen cafeteria?'))
+cost_lunch = float(input('The price of a typical student lunch?'))
+spent_groceries = float(input('How much money do you spend on groceries in a week?'))
+
+spent_weekly = (eat_lunch * cost_lunch) + spent_groceries
+
+print()
+print('Average food expenditure:')
+print(f'Daily: {spent_weekly / 7} euros')
+print(f'Weekly: {spent_weekly} euros')
--- part01-20_students_in_groups/src/students_in_groups.py
+++ part01-20_students_in_groups/src/students_in_groups.py
@@ -1,1 +1,14 @@
 # Write your solution here
+count_student = int(input('How many students on the course?'))
+group_size = int(input('Desired group size?'))
+
+
+group_formed = 0
+if count_student < group_size:
+    group_formed = 1
+elif (count_student / group_size) % 2 != 0:
+    group_formed = (count_student / group_size) + 1
+else:
+    group_formed = (count_student / group_size)
+    
+print(f'Number of groups formed: {int(group_formed)}')
