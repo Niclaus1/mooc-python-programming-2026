888b2a4524b1050210f084d4e31501d2274368b1
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-14 23:41:02 +0800

    Exercise Completed

--- part01-27_daily_wages/src/daily_wages.py
+++ part01-27_daily_wages/src/daily_wages.py
@@ -1,1 +1,12 @@
 # Write your solution here
+hour_wage = float(input('Hourly wage:'))
+hour_worked = float(input('Hours worked:'))
+day_of_week = input('Day of the week:')
+
+daily_wage = 0
+if day_of_week == 'Sunday':
+    daily_wage = (hour_wage * hour_worked) * 2
+else:
+    daily_wage = (hour_wage * hour_worked) 
+
+print(f'Daily wages: {daily_wage} euros')
