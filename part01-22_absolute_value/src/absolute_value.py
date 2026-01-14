7adc6f3717fd6eefe748e1785ba433fe44d9dfc4
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-14 23:24:26 +0800

    Exercise Completed

--- part01-22_absolute_value/src/absolute_value.py
+++ part01-22_absolute_value/src/absolute_value.py
@@ -1,1 +1,10 @@
 # Write your solution here
+number_input = int(input('Please type in a number:'))
+
+absolute_val = 0
+if number_input < 0:
+    absolute_val = number_input * -1
+else:
+    absolute_val = number_input
+
+print(f'The absolute value of this number is {absolute_val}')
--- part01-23_soup_or_no_soup/src/soup_or_no_soup.py
+++ part01-23_soup_or_no_soup/src/soup_or_no_soup.py
@@ -1,1 +1,10 @@
 # Write your solution here
+name = input('Please tell me your name:')
+
+if name == 'Jerry':
+    print('Next please!')
+else:
+    soup_portion = int(input('How many portions of soup?'))
+    print(f'The total cost is {soup_portion * 5.90}')
+    print('Next please!')
+    
