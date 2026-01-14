aea5a1a273c9798a21c991668648071ed5c90055
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-14 22:15:34 +0800

    Exercise Completed

--- part01-18_sum_and_mean/src/sum_and_mean.py
+++ part01-18_sum_and_mean/src/sum_and_mean.py
@@ -1,1 +1,10 @@
 # Write your solution here
+count_total = 4
+
+number = 0
+number += int(input('Number 1:'))
+number += int(input('Number 2:'))
+number += int(input('Number 3:'))
+number += int(input('Number 4:'))
+
+print(f'The sum of the numbers is {number} and the mean is {number / count_total} ')
