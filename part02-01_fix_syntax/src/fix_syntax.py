d2752d09237136ec4d77730eefcc73cda16a163f
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-15 10:58:22 +0800

    Exercise Completed

--- part02-01_fix_syntax/src/fix_syntax.py
+++ part02-01_fix_syntax/src/fix_syntax.py
@@ -1,21 +1,12 @@
 # Fix the program
-# number = int(input("Please type in a number: "))
+number = int(input("Please type in a number: "))
   
-# if number > 100:
-#   print("The number was greater than one hundred")
-#   number -= 100
-#   print("Now its value has decreased by one hundred")
+if number > 100:
+  print("The number was greater than one hundred")
+  number -= 100
+  print("Now its value has decreased by one hundred")
         
-#   print("Its value is now " + str(number))
+  print("Its value is now " + str(number))
   
-# print(str(number) + " must be my lucky number!")
-# print("Have a nice day!")
-
-  number = input("Please type in a number: ")
-  if number>100
-    print("The number was greater than one hundred")
-    number - 100
-    print("Now its value has decreased by one hundred)
-     print("Its value is now"+ number)
- print(number + " must be my lucky number!")
- print("Have a nice day!)
+print(str(number) + " must be my lucky number!")
+print("Have a nice day!")
