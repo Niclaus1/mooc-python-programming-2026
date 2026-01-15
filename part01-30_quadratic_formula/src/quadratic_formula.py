f80569952fb55a2be5ec26ca20b8bba3793a190c
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-15 10:20:19 +0800

    Exercise Completed

--- part01-30_quadratic_formula/src/quadratic_formula.py
+++ part01-30_quadratic_formula/src/quadratic_formula.py
@@ -4,3 +4,12 @@ from math import sqrt
 
 # Note that the square root can also be calculated using power.
 # sqrt(9) is equivalent to 9 ** 0.5
+valueA = float(input('Values of a:'))
+valueB = float(input('Values of b:'))
+valueC = float(input('Values of c:'))
+
+
+valueNeg = ((valueB * -1) - sqrt((valueB * valueB) - (4 * valueA * valueC))) / (2 * valueA)
+ValuePos = ((valueB * -1) + sqrt((valueB * valueB) - (4 * valueA * valueC))) / (2 * valueA)
+
+print(f'The roots are {ValuePos} and {valueNeg}')
