b1c90c17ec7127c50aa92ee4eb6b23ef6ff01531
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-17 18:06:41 +0800

    Exercise Completed

--- part02-21_story/src/story.py
+++ part02-21_story/src/story.py
@@ -1,1 +1,13 @@
 # Write your solution here
+string = ""
+last_word = ""
+while True:
+    word = input('Please type in a word:')
+    
+    if word == 'end' or word == last_word:
+        print(string)
+        break
+    
+    string = string + word + ' '   
+    last_word = word
+    
