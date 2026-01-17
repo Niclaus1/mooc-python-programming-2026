cb5fc83bb70575dc9349a68d81c9b428b430cb8d
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-17 17:33:27 +0800

    Exercise Completed

--- part02-20_next_leap_year/src/next_leap_year.py
+++ part02-20_next_leap_year/src/next_leap_year.py
@@ -1,1 +1,11 @@
 # Write your solution here
+
+this_year = int(input('Year:'))
+next_year = this_year + 1
+
+while True:
+    if (next_year % 4 == 0 and next_year % 100 != 0) or next_year % 400 == 0:
+        print(f'The next leap year after {this_year} is {next_year}')
+        break
+    else:
+        next_year += 1
