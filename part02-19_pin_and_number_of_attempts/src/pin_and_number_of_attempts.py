13e0a55c663b5b2e8ca36a1213fd981fea702b37
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-17 16:26:49 +0800

    Exercise Completed

--- part02-19_pin_and_number_of_attempts/src/pin_and_number_of_attempts.py
+++ part02-19_pin_and_number_of_attempts/src/pin_and_number_of_attempts.py
@@ -1,1 +1,15 @@
 # Write your solution here
+correct_password = '4321'
+attempt = 1
+while True:
+    input_password = input('PIN:')
+    if input_password == correct_password:
+        if attempt == 1:
+            print('Correct! It only took you one single attempt!')
+            break
+        print(f'Correct! It took you {attempt} attempts')
+        break
+    else:
+        print('Wrong')
+        attempt += 1
+        
