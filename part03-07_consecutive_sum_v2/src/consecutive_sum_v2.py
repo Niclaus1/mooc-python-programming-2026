0ccb040e58c6032d7d91239f276b2ed4dc9eaf9a
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-18 23:46:46 +0800

    Exercise Completed

--- part03-07_consecutive_sum_v2/src/consecutive_sum_v2.py
+++ part03-07_consecutive_sum_v2/src/consecutive_sum_v2.py
@@ -1,1 +1,16 @@
 # Write your solution here
+upper_limit = int(input('Limit:'))
+
+current_number = 0
+count = 1
+string = 'The consecutive sum: '
+
+while current_number < upper_limit:
+    string += f'{count}'
+    current_number += count
+    count += 1
+    
+    if current_number < upper_limit:
+        string += " + "
+
+print(f'{string} = {current_number}')
