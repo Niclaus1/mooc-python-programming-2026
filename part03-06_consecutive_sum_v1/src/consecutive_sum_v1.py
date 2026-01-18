046f3067c043ceb1a8605b0e99e900b186a35bf5
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-18 23:35:41 +0800

    Exercise Completed

--- part03-06_consecutive_sum_v1/src/consecutive_sum_v1.py
+++ part03-06_consecutive_sum_v1/src/consecutive_sum_v1.py
@@ -1,1 +1,10 @@
 # Write your solution here
+upper_limit = int(input('Limit:'))
+
+current_number = 0
+count = 1
+
+while current_number < upper_limit:
+    current_number += count
+    count += 1  
+print(current_number)
