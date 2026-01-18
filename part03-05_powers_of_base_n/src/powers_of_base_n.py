28758bbb81f32a10755d73b5fb012836ef870938
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-18 20:13:52 +0800

    Exercise Completed

--- part03-05_powers_of_base_n/src/powers_of_base_n.py
+++ part03-05_powers_of_base_n/src/powers_of_base_n.py
@@ -1,1 +1,11 @@
 # Write your solution here
+upper_limit = int(input('Upper limit:'))
+base = int(input('Base:'))
+
+current_number = 1
+
+while current_number <= upper_limit:
+    print(current_number)
+
+    current_number *= base
+    
