ab8fe06cf0a5da31f3d3231bc015dd53eb06072a
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-19 16:39:41 +0800

    Exercise Completed

--- part03-15_right_aligned/src/right_aligned.py
+++ part03-15_right_aligned/src/right_aligned.py
@@ -1,1 +1,8 @@
 # Write your solution here
+string = input('Please type in a sting:')
+stirng_len = len(string)
+char = '*'
+char_default = 20
+char_modified = char_default - stirng_len
+
+print(f'{char * char_modified}{string}')
