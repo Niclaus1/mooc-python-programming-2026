765641e6c0326cc51a2ea7a2bbaa8b313be78148
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-19 16:35:24 +0800

    Exercise Completed

--- part03-14_underlining/src/underlining.py
+++ part03-14_underlining/src/underlining.py
@@ -1,1 +1,15 @@
 # Write your solution here
+
+
+while True:
+    string = input('Please type in a string:')
+    string_len = len(string)
+    dash = '-'
+    print()
+    
+    if len(string) <= 0:
+        break
+    else:
+        print(string)
+        print(string_len * dash)
+    print()
