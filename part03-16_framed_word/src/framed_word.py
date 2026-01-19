84f5437470a3e2de7a843bccb9b4ea262ab89011
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-19 23:06:37 +0800

    Exercise Completed

--- part03-16_framed_word/src/framed_word.py
+++ part03-16_framed_word/src/framed_word.py
@@ -1,1 +1,17 @@
 # Write your solution here
+word = input('Word:')
+char = '*'
+space = ' '
+
+word_len = len(word)
+space_need = (28 - word_len) // 2
+
+if word_len % 2 == 0:
+    print(char * 30)
+    print(f'{char}{space * space_need}{word}{space * space_need}{char}')
+    print(char * 30)
+else:
+    print(char * 30)
+    print(f'{char}{space * space_need}{word}{space * (space_need + 1)}{char}')
+    print(char * 30)
+    
