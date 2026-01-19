1e7ce280d36499c97c3de5384bf0574365efb510
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-19 16:14:09 +0800

    Exercise Completed

--- part03-10_end_to_beginning/src/end_to_beginning.py
+++ part03-10_end_to_beginning/src/end_to_beginning.py
@@ -1,1 +1,10 @@
 # Write your solution here
+string = input('Please type in a string:')
+
+index = -1
+count = 0
+
+while count < len(string):
+    print(string[index])
+    index -= 1
+    count += 1
