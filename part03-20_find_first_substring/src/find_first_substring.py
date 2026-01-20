5ec742926d3a0be4af034153e1c5e5a06addb7c6
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-20 10:32:03 +0800

    Exercise Completed

--- part03-20_find_first_substring/src/find_first_substring.py
+++ part03-20_find_first_substring/src/find_first_substring.py
@@ -1,1 +1,14 @@
 # Write your solution here
+word = input('Please type in a word:')
+character = input('Please type in a character:')
+index = word.find(character)
+
+if len(word) < 3:
+    print()
+elif index == -1:
+    print()
+elif (index + 3) > len(word):
+    print()
+else:
+    print(word[index : index + 3])
+    
