7fea6b4beba47e3ab6b04a0f07b770721f915431
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-20 12:15:28 +0800

    Exercise Completed

--- part03-22_second_occurrence/src/second_occurrence.py
+++ part03-22_second_occurrence/src/second_occurrence.py
@@ -1,1 +1,12 @@
 # Write your solution here
+word = input('Please type in a string:')
+substring = input('Please type in a substring:')
+
+
+index = word.find(substring)
+index2 = word.find(substring, index + len(substring))
+
+if index == -1 or index2 == -1:
+    print('The substring does not occur twice in the string.')
+else:
+    print(f'The second occurrence of the substring is at index {index2}.')
