31004dc719c5a5cfb3e4611249a119d18ccc2d4e
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-20 10:44:49 +0800

    Exercise Completed

--- part03-21_find_all_substrings/src/find_all_substrings.py
+++ part03-21_find_all_substrings/src/find_all_substrings.py
@@ -1,1 +1,16 @@
 # Write your solution here
+word = input('Please type in a word:')
+character = input('Please type in a character:')
+
+while True:
+    index = word.find(character)
+    
+    if len(word) < 3:
+        break
+    elif index == -1:
+        break
+    elif (index + 3) > len(word):
+        break
+    
+    print(word[index: index + 3])
+    word = word[index + 1:]
