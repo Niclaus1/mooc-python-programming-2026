f7a7fd15192675febbc4ca1617754b64a36bf7fe
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-21 10:51:16 +0800

    Exercise Completed

--- part03-24_first_letters_of_words/src/first_letters_of_words.py
+++ part03-24_first_letters_of_words/src/first_letters_of_words.py
@@ -1,1 +1,10 @@
 # Write your solution here
+# word = input('Please type in a sentence:')
+word = 'Hello Nick'
+word_len = len(word)
+
+step = 0
+while step < word_len:
+    print
+
+
--- part03-25_factorial/src/factorial.py
+++ part03-25_factorial/src/factorial.py
@@ -1,1 +1,15 @@
 # Write your solution here
+while True:
+    number = int(input('Please type in a number:'))
+
+    if number <= 0 or number == -1:
+        print('Thanks and bye!')
+        break
+    
+    count = 1
+    value = 1
+    while count < number:
+        value *= (count + 1) 
+        count += 1
+
+    print(f'The factorial of the number {number} is {value}')
