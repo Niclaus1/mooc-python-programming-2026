9216c0492f44ddb80c5dacb69c331d0789fee08d
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-21 10:04:10 +0800

    Exercise Completed

--- part03-23_multiplication/src/multiplication.py
+++ part03-23_multiplication/src/multiplication.py
@@ -1,1 +1,12 @@
 # Write your solution here
+number = int(input('Please type in a number:'))
+
+count = 1
+while count < (number + 1):
+    step = 1
+    
+    while step < (number + 1):
+        print(f'{count} x {step} = {count * step}')
+        step += 1
+        
+    count += 1
