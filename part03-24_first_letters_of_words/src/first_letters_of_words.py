c9ec4a9be89b86622b54f86714c86ade58035bab
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-21 11:21:49 +0800

    Exercise Completed

--- part03-24_first_letters_of_words/src/first_letters_of_words.py
+++ part03-24_first_letters_of_words/src/first_letters_of_words.py
@@ -1,10 +1,12 @@
 # Write your solution here
-# word = input('Please type in a sentence:')
-word = 'Hello Nick'
+word = input('Please type in a sentence:')
 word_len = len(word)
 
 step = 0
+print(word[0])  
+
 while step < word_len:
-    print
-
-
+    
+    if word[step] == ' ' and (step + 1 < word_len):
+        print(word[step + 1])
+    step += 1
