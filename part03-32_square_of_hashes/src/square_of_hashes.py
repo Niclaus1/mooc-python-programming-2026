bd881d24e8049ec8f59d088ee8b9f9f5ca6860dd
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-23 23:53:57 +0800

    Exercise Completed

--- part03-32_square_of_hashes/src/square_of_hashes.py
+++ part03-32_square_of_hashes/src/square_of_hashes.py
@@ -1,4 +1,11 @@
 # Write your solution here
+def hash_square(count):
+    char = "#"
+    step = 0
+    
+    while step < count:
+        print(char * count)
+        step += 1
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     hash_square(5)
