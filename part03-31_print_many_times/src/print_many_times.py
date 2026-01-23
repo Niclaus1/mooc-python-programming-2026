89897f599ee9f4d5553920f07bcd56b6665b33e4
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-23 23:52:17 +0800

    Exercise Completed

--- part03-31_print_many_times/src/print_many_times.py
+++ part03-31_print_many_times/src/print_many_times.py
@@ -1,5 +1,9 @@
 # Write your solution here
-
+def print_many_times(word, count):
+    step = 0
+    while step < count:
+        print(word)
+        step += 1
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     print_many_times("python", 5)
