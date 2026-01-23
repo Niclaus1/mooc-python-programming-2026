dfbc00be8f8dbc1ff26d9f3d1792a14da0a00b5f
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-24 00:36:29 +0800

    Exercise Completed

--- part03-33_chessboard/src/chessboard.py
+++ part03-33_chessboard/src/chessboard.py
@@ -1,5 +1,24 @@
 # Write your solution here
+def chessboard(count):
+    step = 0
+    while step < count:
+        state = 0
+        
+        if step % 2 == 0:
+            state = 1
+        i = 0
+        
+        while i < count:
+            print(state, end="")
+            if state == 1:
+                state = 0
+            else:
+                state = 1
+            i += 1
+        
+        print()
+        step += 1
 
 # Testing the function
 if __name__ == "__main__":
-    chessboard(3)
+    chessboard(4)
