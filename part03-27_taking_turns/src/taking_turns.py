7fd3b20417608a219ab4806a55ca3f4e5d40189b
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-23 10:20:23 +0800

    Exercise Completed

--- part03-27_taking_turns/src/taking_turns.py
+++ part03-27_taking_turns/src/taking_turns.py
@@ -1,1 +1,31 @@
 # Write your solution here
+
+number = int(input('Please type in a number:'))
+
+countUp = 1
+countDown = number
+
+"""My Solution"""
+while countUp < countDown:
+    print(countUp)
+    print(countDown)
+
+    countUp += 1
+    countDown -= 1
+
+if number == 3:
+    print(number - 1)
+elif number != 3 and number % 2 != 0:
+    print(number - (countUp -1))
+
+
+# """Other Solution"""
+
+# while countUp <= countDown:
+#     print(countUp)
+
+#     if countUp != countDown:
+#         print(countDown)
+    
+#     countUp += 1
+#     countDown -= 1
---
+++ part03-27_taking_turns/src/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+if number % 2 != 0:
+    print(number)
+else:
+    print(number)
