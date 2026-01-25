88d4ac7587acfdfc062a14cbaa7be23dca2a9f53
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-25 20:19:38 +0800

    Exercise Completed

--- part04-07_shape/src/shape.py
+++ part04-07_shape/src/shape.py
@@ -1,4 +1,22 @@
 # Copy here code of line function from previous exercise and use it in your solution
+def line(triangle_number, triangle_string, square_number, square_string):
+    tri_rows = 1
+    
+    while tri_rows < triangle_number + 1:
+        print(triangle_string * tri_rows)
+        tri_rows += 1
+    
+    squ_rows = 0
+    while squ_rows < square_number:
+        if square_number <= 0:
+            break
+        print(square_string * triangle_number)
+        squ_rows += 1
+
+
+def shape(triangle_number, triangle_rows, square_number, square_string):
+    line(triangle_number, triangle_rows, square_number, square_string)
+    
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
-    shape(5, "x", 2, "o")
+    shape(3, ".", 0, ",")
