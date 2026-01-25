f53b38e5badc8b8d74698eafe36aff1a0a0e2560
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-25 21:14:24 +0800

    Exercise Completed

--- part04-08_spruce/src/spruce.py
+++ part04-08_spruce/src/spruce.py
@@ -1,4 +1,18 @@
 # Write your solution here
+def spruce(number):
+    row = 1
+    
+    space = ' '
+    char = '*'
+    
+    print('a spruce!')
+    while row < number + 1:
+        print(space * (number - row) + char * (row) + char * (row - 1))
+        row += 1
+
+    print(space * (number - 2), char)
+
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
-    spruce(5)
+    spruce(10)
