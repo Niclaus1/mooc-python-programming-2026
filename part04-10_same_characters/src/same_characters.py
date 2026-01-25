efc6b9d922e3b64dd0ee0e4f65f1891fdae4d27e
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-25 23:53:27 +0800

    Exercise Completed

--- part04-10_same_characters/src/same_characters.py
+++ part04-10_same_characters/src/same_characters.py
@@ -1,4 +1,14 @@
 # Write your solution here
+def same_chars(string: str, a: int, b: int):
+    
+    if len(string) - 1 < a or len(string) - 1 < b:
+        return False
+    else:
+        return string[a] == string[b]
+    
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     print(same_chars("coder", 1, 2))
+    print(same_chars("programmer", 6, 7))
+    print(same_chars("programmer", 0, 4))
+    print(same_chars("abc", 0, 3))
