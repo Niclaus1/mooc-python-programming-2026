587e29c0346f5550e4ac533e07695d23e1c18bfe
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-25 17:54:48 +0800

    Exercise Completed

--- part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
+++ part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
@@ -1,1 +1,11 @@
 # Write your solution here
+
+while True:
+    word = input('Editor:')
+    if word.lower() == 'visual studio code':
+        print('an excellent choice!')
+        break
+    elif word.lower() == 'vim' or word.lower() == 'word' or word.lower() == 'notepad':
+        print('awful')
+    else:
+        print('not good')   
