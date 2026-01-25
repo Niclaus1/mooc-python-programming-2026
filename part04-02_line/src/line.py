74d9672964f2cc712f84298a2257dbd9657b2d61
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-25 19:50:06 +0800

    Exercise Completed

--- part04-02_line/src/line.py
+++ part04-02_line/src/line.py
@@ -1,4 +1,14 @@
 # Write your solution here
+def line(number, string):
+    count = 0
+    while count < number:
+        if string == "":
+            print('*' ,end="")
+            count += 1
+        else:
+            print(string[0], end="")
+            count += 1
+    
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
-    line(5, "x")
+    line(5, "")
