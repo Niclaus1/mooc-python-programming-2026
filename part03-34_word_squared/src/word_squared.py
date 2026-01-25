86a64e174772d0565112c763c836427ff64e0c72
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-25 14:07:14 +0800

    Exercise Completed

--- part03-34_word_squared/src/word_squared.py
+++ part03-34_word_squared/src/word_squared.py
@@ -1,1 +1,23 @@
 # Write your solution here
+def squared (string, number):
+
+    row = 0
+    
+    index = 0
+    while row < number:
+        col = 0 
+        
+        while col < number:
+            print(string[index], end= "")
+            
+            index += 1
+            
+            if index >= len(string):
+                index = 0
+            col += 1
+        print()
+        
+        row += 1
+
+if __name__ == "__main__":
+    squared('niclaus' , 3)
