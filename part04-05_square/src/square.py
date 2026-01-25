ab53093cea24ef983349cbafab514e74394dd5a0
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-25 20:04:18 +0800

    Exercise Completed

--- part04-05_square/src/square.py
+++ part04-05_square/src/square.py
@@ -1,8 +1,20 @@
 # Copy here code of line function from previous exercise
+def line(number, string):
+    row = 0
+    
+    while row < number:
+        col = 0
+        
+        while col < number:
+            print(string, end="")
+
+            col += 1
+        print()
+        row += 1
 
 def square(size, character):
     # You should call function line here with proper parameters
-    line(4, "x")
+    line(size, character)
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
