c0faa3cfba4eadb50fc7a27a53ce11483d8d45bf
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-25 19:57:59 +0800

    Exercise Completed

--- part04-03_box_of_hashes/src/box_of_hashes.py
+++ part04-03_box_of_hashes/src/box_of_hashes.py
@@ -1,8 +1,14 @@
 # Copy here code of line function from previous exercise
-
+def line(number, string):
+    count = 0
+    
+    while count < number:
+        print(string * 10)
+        count += 1
+    
 def box_of_hashes(height):
     # You should call function line here with proper parameters
-    line(10, "#")
+    line(height, "#")
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
