89b1bb5f3843b6770b5d0b7bdf62be9da14d103a
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-26 10:47:48 +0800

    Exercise Completed

--- part04-13_add_items_to_list/src/add_items_to_list.py
+++ part04-13_add_items_to_list/src/add_items_to_list.py
@@ -1,1 +1,10 @@
 # Write your solution here
+items = int(input('How many items:'))
+list = []
+count = 0
+
+while count < items:
+    item = int(input(f'Item {count + 1}:'))
+    list.append(item)
+    count += 1
+print(list)
