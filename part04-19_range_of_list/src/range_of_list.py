cbfa5f987a88a0c42f1fe65ae822dcf44d1a58b1
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-26 22:44:57 +0800

    Exercise Completed

--- part04-19_range_of_list/src/range_of_list.py
+++ part04-19_range_of_list/src/range_of_list.py
@@ -1,6 +1,11 @@
 # Write your solution here
+def range_of_list(list : list):
+    maxValue = max(list)
+    minValue = min(list)
+
+    return maxValue - minValue
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
-    my_list = [3, 6, -4]
+    my_list = [1, 2, 3, 4, 5]
     result = range_of_list(my_list)
     print(result)
