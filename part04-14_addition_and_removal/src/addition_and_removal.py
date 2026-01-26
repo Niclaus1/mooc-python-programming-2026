8cc96001d443e48b1dc8d726efeef19af74530fd
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-26 12:10:50 +0800

    Exercise Completed

--- part04-14_addition_and_removal/src/addition_and_removal.py
+++ part04-14_addition_and_removal/src/addition_and_removal.py
@@ -1,1 +1,21 @@
 # Write your solution here
+list = []
+count = 0
+while True:
+    print(f'The list is now {list}')            
+    menu = input('a(d)d, (r)emove or e(x)it:')
+
+    if menu.lower() == 'd':
+        count += 1
+        list.append(count)
+        
+    elif menu.lower() == 'r':
+        if len(list) > 0:
+            list.pop()
+            count -= 1
+        else:
+            continue
+        
+    elif menu.lower() == 'x':
+        print('Bye!')
+        break
