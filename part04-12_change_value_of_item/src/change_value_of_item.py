2e10a9b743761f94f292cb1e52e9cd762544424c
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-26 10:37:37 +0800

    Exercise Completed

--- part04-12_change_value_of_item/src/change_value_of_item.py
+++ part04-12_change_value_of_item/src/change_value_of_item.py
@@ -1,1 +1,13 @@
 # Write your solution here
+numbers = [1, 2, 3, 4, 5]
+
+while True:
+    index = int(input('Index:'))
+    
+    if index < 0:
+        break
+    
+    number = int(input('New value:'))
+
+    numbers[index] = number
+    print(numbers)
