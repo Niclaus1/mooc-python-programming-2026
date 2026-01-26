3582888fddb9d27116cd46ed3e8f2f836341e89f
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-26 22:39:55 +0800

    Exercise Completed

--- part04-18_mean/src/mean.py
+++ part04-18_mean/src/mean.py
@@ -1,6 +1,17 @@
 # Write your solution here
+def mean(list: list):
+    count = len(list)
+    
+    value = 0
+    index = 0
+    
+    while index < count:
+        value += list[index]
+        index += 1
+    return value / count
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
-    my_list = [3, 6, -4]
+    my_list = [1, 2, 3, 4, 5]
     result = mean(my_list)
     print(result)
