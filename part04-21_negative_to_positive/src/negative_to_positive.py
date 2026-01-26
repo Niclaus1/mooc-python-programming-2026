1ac21dac787c2d27c64e9ca32bf551f5f6bd0bec
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-26 23:17:27 +0800

    Exercise Completed

--- part04-21_negative_to_positive/src/negative_to_positive.py
+++ part04-21_negative_to_positive/src/negative_to_positive.py
@@ -1,1 +1,8 @@
 # Write your solution here
+number = int(input('Please type in a positive integer:'))
+
+for i in range((number * -1),number + 1):
+    if i == 0:
+        continue
+    print(i)
+    
