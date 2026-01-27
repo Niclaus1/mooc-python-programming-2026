e3882abb7e4c1f0ed9c9c9bd628bb26f86c55a38
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-27 13:36:30 +0800

    Exercise Completed

--- part04-29_length_of_longest/src/length_of_longest.py
+++ part04-29_length_of_longest/src/length_of_longest.py
@@ -1,1 +1,13 @@
 # Write your solution here
+def length_of_longest(list : list):
+    longest_length = []
+
+    for word in list:
+        longest_length.append(len(word))
+
+    return max(longest_length)
+
+if __name__ == "__main___":
+    my_list = ["first", "second", "fourth", "eleventh"]
+    result = length_of_longest(my_list)
+    print(result)
