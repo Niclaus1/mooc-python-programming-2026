32782ca5752554befd50dff89932b26fbdf60d78
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-27 13:55:00 +0800

    Exercise Completed

--- part04-30_shortest_in_list/src/shortest_in_list.py
+++ part04-30_shortest_in_list/src/shortest_in_list.py
@@ -1,1 +1,15 @@
 # Write your solution here
+def shortest(list : list):
+    shortest_string = list[0]
+
+    for word in list:
+        if len(word) < len(shortest_string):
+            shortest_string = word
+
+    return shortest_string
+
+if __name__ == "__main__":
+        
+    my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
+    result = shortest(my_list)
+    print(result)
