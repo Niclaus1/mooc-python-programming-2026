feaa3ebb230a66dbfd0ef7d979e79fd0b8c0a2e2
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-28 23:27:41 +0800

    Exercise Completed

--- part04-36_no_shouting_allowed/src/no_shouting_allowed.py
+++ part04-36_no_shouting_allowed/src/no_shouting_allowed.py
@@ -1,1 +1,17 @@
 # Write your solution here
+def no_shouting (list : list):
+    lowered_list = []
+    
+    for word in list:
+        if word.isupper():
+            continue
+        else:
+            lowered_list.append(word)
+
+
+    return lowered_list
+
+if __name__ == "__main__":
+    my_list = ["ABC", "def", "UPPER", "ANOTHERUPPER", "lower", "another lower", "Capitalized"]
+    pruned_list = no_shouting(my_list)
+    print(pruned_list)
