ae7ef1cdd4085c0a86f27037393e071d4c0b574f
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-28 23:20:54 +0800

    Exercise Completed

--- part04-35_no_vowels_allowed/src/no_vowels_allowed.py
+++ part04-35_no_vowels_allowed/src/no_vowels_allowed.py
@@ -1,1 +1,12 @@
 # Write your solution here
+def no_vowels(word : str):
+    vowels = ['a','e','i','o','u']
+
+    string = word
+    for vowel in vowels:
+        string = string.replace(vowel, "")
+    
+    return string
+if __name__ == "__main__":
+    my_string = "this is an example"
+    print(no_vowels(my_string))
