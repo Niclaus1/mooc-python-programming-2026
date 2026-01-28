deeed93064ce23d7d3fe8bb107ef80fa36458160
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-28 23:18:25 +0800

    Exercise Completed

--- part04-34_most_common_character/src/most_common_character.py
+++ part04-34_most_common_character/src/most_common_character.py
@@ -1,1 +1,14 @@
 # Write your solution here
+def most_common_character(word : str):
+
+    previous_max = word[0]
+
+    for char in word:
+        if word.count(char) > word.count(previous_max):
+            previous_max = char
+
+    return previous_max
+
+if __name__ == "__main__":
+    first_string = "abcdbde"
+    print(most_common_character(first_string))
