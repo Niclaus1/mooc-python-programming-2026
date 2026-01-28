6985ce4a7ce7f4dcbc7fdb90a1dc03e234ff147d
Niclaus1 <rendonniclaus111@gmail.com>
2026-01-28 22:12:42 +0800

    Exercise Completed

--- part04-33_everything_reversed/src/everything_reversed.py
+++ part04-33_everything_reversed/src/everything_reversed.py
@@ -1,1 +1,13 @@
 # Write your solution here
+def everything_reversed(list : list):
+    reversed_words = []
+
+    for word in list:
+        reversed_words.append(word[::-1])
+
+    return reversed_words[::-1]
+
+if __name__ == "__main__":
+    my_list = ["Hi", "there", "example", "one more"]
+    new_list = everything_reversed(my_list)
+    print(new_list)
---
+++ part04-33_everything_reversed/src/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+ything_reversed(my_list)
+    print(new_list)
