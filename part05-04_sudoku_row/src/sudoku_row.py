01dcf12085b02e27af5cc6d4a4a8730989ab7e3b
Niclaus1 <rendonniclaus111@gmail.com>
2026-02-01 12:43:58 +0800

    Exercise Completed

--- part05-04_sudoku_row/src/sudoku_row.py
+++ part05-04_sudoku_row/src/sudoku_row.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def row_correct(sudoku: list, row_no : int):
+    
+    row_count = []
+    for col in range(1, len(sudoku)):
+        row_count.append(sudoku[row_no].count(col))
+
+    for count in row_count:
+        if count > 1:
+            return False
+    return True
