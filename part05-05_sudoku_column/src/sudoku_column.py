fe22a31f290a0e60e369395999f74867bd24bc62
Niclaus1 <rendonniclaus111@gmail.com>
2026-02-02 10:38:12 +0800

    Exercise Completed

--- part05-05_sudoku_column/src/sudoku_column.py
+++ part05-05_sudoku_column/src/sudoku_column.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def column_correct(sudoku: list, column_no: int):
+
+    col_count = []
+    for row in range(len(sudoku)):
+        col_count.append(sudoku[row][column_no])
+    
+    for duplicate in col_count:
+        if duplicate > 0 and col_count.count(duplicate) > 1:
+            return False
+    return True
