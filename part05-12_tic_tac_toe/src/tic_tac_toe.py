ead633fb76d059ce31c3a87bcd5ca8bdf1ea3ccf
Niclaus1 <rendonniclaus111@gmail.com>
2026-02-05 23:30:02 +0800

    Exercise Completed

--- part05-12_tic_tac_toe/src/tic_tac_toe.py
+++ part05-12_tic_tac_toe/src/tic_tac_toe.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def play_turn(game_board: list, x: int,y:int, piece: str):
+    if (x < 3 and x >= 0) and (y < 3 and y >=0) and game_board[y][x] == "" :
+        game_board[y][x] = piece
+        return True
+    return False
+
+if __name__ == "__main__":
+    game_board = [["", "", ""], ["O", "O", ""], ["", "", ""]]
+    print(play_turn(game_board, 2, -1, "X"))
+    print(game_board)
