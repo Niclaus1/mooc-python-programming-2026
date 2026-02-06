0c82fbf0e99027d73e59ec4cf4ca662dca9ad937
Niclaus1 <rendonniclaus111@gmail.com>
2026-02-06 11:36:50 +0800

    Exercise Completed

---
+++ part05-13_transpose_matrix/src/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+rix[row][col]
+                matrix[row][col] = matrix[col][row]
+                matrix[col][r
--- part05-13_transpose_matrix/src/transpose_matrix.py
+++ part05-13_transpose_matrix/src/transpose_matrix.py
@@ -1,1 +1,17 @@
 # Write your solution here
+def transpose(matrix: list):
+    for row in range(len(matrix)):
+        for col in range(len(matrix)):
+            if row > col:
+                ref_var = matrix[row][col]
+                matrix[row][col] = matrix[col][row]
+                matrix[col][row] = ref_var
+
+if __name__ == "__main__":
+    matrix = [[1,2,3],
+        [4,5,6],
+        [7,8,9]]
+
+    transpose(matrix)
+    for row in matrix:
+        print(row)
