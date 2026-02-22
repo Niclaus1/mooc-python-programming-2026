62582cb3f660d824a8d60e6c6ec85185057381a4
Niclaus1 <rendonniclaus111@gmail.com>
2026-02-22 18:44:39 +0800

    Exercise Completed

--- part05-16_histogram/src/histogram.py
+++ part05-16_histogram/src/histogram.py
@@ -1,1 +1,18 @@
 # Write your solution here
+
+def histogram(word : str):
+    dict = {}
+    star = "*"
+
+    for char in word:
+        if char not in dict:
+            dict[char] = ""
+        dict[char] = dict[char] + star
+
+    for key, value in dict.items():
+        print(f'{key} {value}')
+
+if __name__ == "__main__":
+    histogram("abba")
+
+
