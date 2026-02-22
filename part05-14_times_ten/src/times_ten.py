d2ce33e7dabc865198420bd05b0256bc342013d2
Niclaus1 <rendonniclaus111@gmail.com>
2026-02-22 15:56:04 +0800

    Exercise Completed

--- part05-14_times_ten/src/times_ten.py
+++ part05-14_times_ten/src/times_ten.py
@@ -1,1 +1,9 @@
 # Write your solution here
+def times_ten(start_index: int, end_index: int):
+    dict = {}
+    for x in range(start_index, end_index + 1):
+        dict[x] = x * 10
+    return dict
+
+if __name__ == "__main__":
+    print(times_ten(1,3))
