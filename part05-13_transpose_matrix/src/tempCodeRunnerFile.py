8028441b9178114ed59dcb22cb82af511c75812d
Niclaus1 <rendonniclaus111@gmail.com>
2026-02-22 15:50:22 +0800

    Exercise Completed

--- part05-13_transpose_matrix/src/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-rix[row][col]
-                matrix[row][col] = matrix[col][row]
-                matrix[col][r
--- part05-13_transpose_matrix/src/transpose_matrix.py
+++ part05-13_transpose_matrix/src/transpose_matrix.py
@@ -15,3 +15,4 @@ if __name__ == "__main__":
     transpose(matrix)
     for row in matrix:
         print(row)
+
