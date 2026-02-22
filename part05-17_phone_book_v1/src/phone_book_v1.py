688bfcef0bd9565a4f1c2a27601f1ab7e632155b
Niclaus1 <rendonniclaus111@gmail.com>
2026-02-22 20:34:19 +0800

    Exercise Completed

--- part05-17_phone_book_v1/src/phone_book_v1.py
+++ part05-17_phone_book_v1/src/phone_book_v1.py
@@ -1,1 +1,32 @@
 # Write your solution here
+def search(dict : dict,name : str):
+    phoneBook = dict
+    if name not in phoneBook:
+        return "no number"
+    return phoneBook[name]
+
+def add(dict : dict,name: str, number : str):
+    phoneBook = dict
+    phoneBook[name] = number
+    return phoneBook
+
+def contact():
+    phoneBook = {}
+    
+    while True:
+        userInput = input("1 search, 2 add, 3 quit: ")
+        
+        if userInput == "1":
+            inputName = input("name: ")
+            print(search(phoneBook,inputName))
+
+        elif userInput == "2":
+            inputName = input("name: ")
+            inputNumber = input("number: ")
+            add(phoneBook, inputName, inputNumber)
+            print("ok!")
+
+        else:
+            print("quitting...")
+            break
+contact()
