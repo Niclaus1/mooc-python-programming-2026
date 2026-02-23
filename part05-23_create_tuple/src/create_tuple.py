df5d613f7c6fa107c18cbaede2b23f94828c47c9
Niclaus1 <rendonniclaus111@gmail.com>
2026-02-23 22:51:46 +0800

    Exercise Completed

--- part05-23_create_tuple/src/create_tuple.py
+++ part05-23_create_tuple/src/create_tuple.py
@@ -1,1 +1,16 @@
-# Write your solution here
+def create_tuple(x : int, y : int, z : int ):
+    lists = [x,y,z]
+    smallest = min(lists)
+    largest = max(lists)
+    sum = x + y + z
+    
+
+        
+    tupled = (smallest,largest,sum)
+    
+
+    return tupled
+    
+
+if __name__ == "__main__":
+    print(create_tuple(1, 4, 2))
