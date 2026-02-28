30bafefab6decf6c13b9013f55258d4ca4f74ba6
Niclaus1 <rendonniclaus111@gmail.com>
2026-02-28 17:30:35 +0800

    Exercise Completed

--- part06-01_largest_number/src/largest_number.py
+++ part06-01_largest_number/src/largest_number.py
@@ -1,2 +1,15 @@
 # write your solution here
+def largest():
+    with open('src/numbers.txt') as new_file:
+        largest_number = 0
+        
+        for line in new_file:
+            line = int(line.replace("\n", ""))
 
+            if largest_number < line:
+                largest_number = line
+
+    return largest_number
+
+if __name__ == "__main__":
+    print(largest())
