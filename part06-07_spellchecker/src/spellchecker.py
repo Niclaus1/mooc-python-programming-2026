9a676e75f105f5cd792d70e46f92392ae63aba9e
Niclaus1 <rendonniclaus111@gmail.com>
2026-03-18 15:47:19 +0800

    Exercise Completed

--- part06-07_spellchecker/src/spellchecker.py
+++ part06-07_spellchecker/src/spellchecker.py
@@ -1,1 +1,20 @@
 # write your solution here
+wordlist = "wordlist.txt"
+
+wordlists = []
+with open(wordlist) as new_file:
+    for line in new_file:
+        # parts = line.split()
+        wordlists.append(line.strip())
+
+
+user_Input = input('Write text: ')
+words = user_Input.split(" ")
+sentence = ""
+
+for word in words:
+    if word.lower() in wordlists:
+        sentence += f' {word}'
+    else:
+        sentence += f' *{word}*'
+print(sentence)
