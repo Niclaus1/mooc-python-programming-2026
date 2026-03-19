e7f535ea0d0e95d82d93ae5c5cb7e42087d25527
Niclaus1 <rendonniclaus111@gmail.com>
2026-03-19 19:56:04 +0800

    Exercise Completed

--- part06-10_inscription/src/inscription.py
+++ part06-10_inscription/src/inscription.py
@@ -1,1 +1,10 @@
 # Write your solution here
+if True:
+    name = input("Whom should I sign this to: ")
+    filename = input("Where shall I save it: ")
+else:
+    name = "Ada"
+    filename =  "inscribed.txt"
+with open(filename,"w") as new_file:
+    new_file.write(f'Hi {name}, we hope you enjoy learning Python with us! Best, Mooc.fi Team')
+    
