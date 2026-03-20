585469e1da55dc94cb2222651f922723b8ff7320
Niclaus1 <rendonniclaus111@gmail.com>
2026-03-20 13:30:48 +0800

    Exercise Completed

--- part06-12_filtering_file_contents/src/filtering_file_contents.py
+++ part06-12_filtering_file_contents/src/filtering_file_contents.py
@@ -5,33 +5,34 @@ def filter_solutions():
     fileName = "solutions.csv"
     correct_list = []
     incorrect_list = []
-    
+
     with open(fileName) as new_file:
         for line in new_file:
             line = line.strip()
             parts = line.split(";")
-            
+
             if "-" in parts[1]:
                 nums = parts[1].split("-")
                 res = int(nums[0]) - int(nums[1])
-                
+
             elif "+" in parts[1]:
                 nums = parts[1].split("+")
                 res = int(nums[0]) + int(nums[1])
-            
+
             if res == int(parts[2]):
                 correct_list.append(line)
             else:
                 incorrect_list.append(line)
 
-    addSolution(fileCorrect,correct_list)
-    addSolution(fileIncorrect,incorrect_list)
+    addSolution(fileCorrect, correct_list)
+    addSolution(fileIncorrect, incorrect_list)
 
 
 def addSolution(filename: str, solution: list):
-    with open(filename, 'w') as write_file:
+    with open(filename, "w") as write_file:
         for sol in solution:
             write_file.write(f"{sol}\n")
+
 
 if __name__ == "__main__":
     filter_solutions()
