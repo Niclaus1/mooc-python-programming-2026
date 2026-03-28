64baace7c3bf6119d7aa3054a4963bb706a5343f
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-03-28 23:38:36 +0800

    Exercise Completed

--- part06-18_parameter_validation/src/parameter_validation.py
+++ part06-18_parameter_validation/src/parameter_validation.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def new_person(name:str, age:int):    
+    wordsLen = name.split()
+    print(len(wordsLen))
+    if name != "" and len(name) < 40 and age > 0 and age < 150 and len(wordsLen) > 1:
+        return (name,age)
+    else:
+        raise ValueError
+
+if __name__ == "__main__":
+    print(new_person("Sirkka-Liisa Virtanen-Aftenbladet-Totterstrom-Lahtiska-Vanamo-Kullervoinen", 32))
