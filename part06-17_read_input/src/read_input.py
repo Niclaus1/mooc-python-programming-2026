4b83ef457addab5a6666b9d02fe80bd4870a43c6
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-03-28 22:38:28 +0800

    Exercise Completed

--- part06-17_read_input/src/read_input.py
+++ part06-17_read_input/src/read_input.py
@@ -1,1 +1,16 @@
 # Write your solution here
+def read_input(userInput:str,minValue:int,maxValue:int):
+    
+    while True:
+        try:
+            userInput = input(userInput)
+            number = int(userInput)
+            if number > minValue and number < maxValue:
+                return number
+        except ValueError:
+            pass
+        print(f"You must type in an integer between {minValue} and {maxValue}")
+
+if __name__ == "__main__":
+    number = read_input("Please type in a number: ", 1,5)
+    print("You typed in:", number)
