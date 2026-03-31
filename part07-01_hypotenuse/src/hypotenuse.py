6aa29c19ca4c21813337dd5b395739646815f6e9
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-03-31 20:58:57 +0800

    Exercise Completed

--- part07-01_hypotenuse/src/hypotenuse.py
+++ part07-01_hypotenuse/src/hypotenuse.py
@@ -1,1 +1,10 @@
 # Write your solution here
+from math import *
+
+def hypotenuse(leg1: float, leg2:float):
+    return sqrt(pow(leg1,2) + pow(leg2,2))
+
+if __name__ == "__main__":
+    print(hypotenuse(3,4)) # 5.0
+    print(hypotenuse(5,12)) # 13.0
+    print(hypotenuse(1,1)) # 1.4142135623730951
