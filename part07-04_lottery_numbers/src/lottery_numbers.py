a1dfc9ce59cec5d8052cea74570e7ce3c2ab75e3
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-04-01 11:14:49 +0800

    Exercise Completed

--- part07-04_lottery_numbers/src/lottery_numbers.py
+++ part07-04_lottery_numbers/src/lottery_numbers.py
@@ -1,1 +1,11 @@
 # Write your solution here
+from random import sample
+
+def lottery_numbers(amount : int, lower : int, upper :int):
+    number_pool = list(range(lower, upper))
+    lot_num = sample(number_pool,amount)
+    
+    return sorted(lot_num)
+if __name__ == "__main__":
+    for number in lottery_numbers(7, 1, 40):
+        print(number)
