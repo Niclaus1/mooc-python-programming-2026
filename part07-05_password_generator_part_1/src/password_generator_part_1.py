f08342240a6d1f40761803953c1d8fc3c6ca29b3
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-04-01 11:24:32 +0800

    Exercise Completed

--- part07-05_password_generator_part_1/src/password_generator_part_1.py
+++ part07-05_password_generator_part_1/src/password_generator_part_1.py
@@ -1,1 +1,10 @@
 # Write your solution here
+import string 
+from random import sample
+
+def generate_password(length : int):
+    return ''.join(sample(string.ascii_lowercase,length))
+
+if __name__ == "__main__":
+    for i in range(10):
+        print(generate_password(8))
