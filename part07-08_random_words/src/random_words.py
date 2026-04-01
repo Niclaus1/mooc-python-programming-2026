a573925fb1e37f074c8b89b54b5df5e45120cca4
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-04-01 18:16:24 +0800

    Exercise Completed

--- part07-08_random_words/src/random_words.py
+++ part07-08_random_words/src/random_words.py
@@ -1,1 +1,17 @@
 # Write your solution here
+from random import sample
+
+def words(n:int, beginning:str):
+    wordMatch = []
+    with open('words.txt') as file:
+        for line in file:
+            line = line.strip()
+            if line.startswith(beginning):
+                wordMatch.append(line)
+    
+    return sample(wordMatch,n)
+
+if __name__ == "__main__":
+    word_list = words(3, "ca")
+    for word in word_list:
+        print(word)
