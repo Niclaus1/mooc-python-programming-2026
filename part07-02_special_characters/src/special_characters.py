1889093900834afcc0588c84645eb25e1663e756
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-04-01 10:06:23 +0800

    Exercise Completed

--- part07-02_special_characters/src/special_characters.py
+++ part07-02_special_characters/src/special_characters.py
@@ -1,1 +1,22 @@
 # Write your solution here
+import string
+
+def separate_characters(my_string: str):
+    letters = ''
+    punctuation = ''
+    other = ''
+    for char in my_string:
+        if char in string.ascii_letters:
+            letters += char
+        elif char in string.punctuation:
+            punctuation += char
+        else:
+            other += char
+
+    return (letters,punctuation,other)
+
+if __name__ == "__main__":
+    parts = separate_characters("Olé!!! Hey, are ümläüts wörking?")
+    print(parts[0])
+    print(parts[1])
+    print(parts[2])
