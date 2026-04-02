e0afb6aeea6b5f3b566de080b9392b211b9167f6
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-04-02 19:41:16 +0800

    Exercise Completed

--- part07-09_how_old/src/how_old.py
+++ part07-09_how_old/src/how_old.py
@@ -1,1 +1,18 @@
 # Write your solution here
+from datetime import datetime
+
+def daysBeforeEveMillennium():
+    try:
+        day = int(input('Day:'))
+        month = int(input('Month:'))
+        year = int(input('Year:'))
+    except ValueError:
+        print('Invalid Input Try again')
+
+    if datetime(year,month,day) > datetime(1999,12,31):
+        print("You weren't born yet on the eve of the new millennium.")
+    else:
+        difference = datetime(1999,12,31) - datetime(year,month,day)
+        print(f"You were {difference.days} days old on the eve of the new millennium.")
+
+daysBeforeEveMillennium()
