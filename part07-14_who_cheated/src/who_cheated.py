6a4849553af269db05584cebe63a702185edde57
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-04-03 20:08:07 +0800

    Exercise Completed

--- part07-14_who_cheated/src/who_cheated.py
+++ part07-14_who_cheated/src/who_cheated.py
@@ -1,1 +1,24 @@
 # Write your solution here
+import csv
+from datetime import timedelta, datetime
+
+def cheaters():
+    threeHours = timedelta(hours=3)
+    students = {}
+    with open('start_times.csv') as start_file:
+        for line in csv.reader(start_file, delimiter=';'):
+            startTime = datetime.strptime(line[1],"%H:%M")
+            students[line[0]] = startTime + threeHours 
+    
+    cheaterList = []
+    with open('submissions.csv') as submission_file:
+        for line in csv.reader(submission_file,delimiter=';'):
+            endTime = datetime.strptime(line[3],"%H:%M") 
+            if line[0] in students and endTime > students[line[0]]:
+                print(line[0], endTime, students[line[0]])
+                if line[0] not in cheaterList:
+                    cheaterList.append(line[0])
+
+    return cheaterList
+if __name__ == "__main__":
+    cheaters()
