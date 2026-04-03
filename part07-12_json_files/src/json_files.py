2ca09c38e788f221c940154aaba812c458155e7e
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-04-03 18:04:32 +0800

    Exercise Completed

--- part07-12_json_files/src/json_files.py
+++ part07-12_json_files/src/json_files.py
@@ -1,1 +1,12 @@
 # Write your solution here
+import json
+
+def print_persons(filename : str):
+    with open(filename) as file:
+        data = file.read()
+
+        names = json.loads(data)
+        for name in names:
+            print(f"{name['name']} {name['age']} years ({', '.join(name['hobbies'])})")
+if __name__ == "__main__":
+    print_persons('file1.json')
