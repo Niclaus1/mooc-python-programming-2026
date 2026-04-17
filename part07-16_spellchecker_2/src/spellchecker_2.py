c3c44b6dbe834849afe3e933304faf1026e44bbc
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-04-17 20:25:26 +0800

    Exercise Completed

--- part07-16_spellchecker_2/src/spellchecker_2.py
+++ part07-16_spellchecker_2/src/spellchecker_2.py
@@ -1,1 +1,30 @@
 # Write your solution here
+import difflib 
+wordtxt = "wordlist.txt"
+
+wordlists = []
+with open(wordtxt) as new_file:
+    for line in new_file:
+        # parts = line.split()
+        wordlists.append(line.strip())
+
+user_Input = input('Write text: ')
+words = user_Input.split(" ")
+sentence = ""
+incorrect_words = []
+
+for word in words:
+    if word.lower() in wordlists:
+        sentence += f' {word}'
+    else:
+        sentence += f' *{word}*'
+        incorrect_words.append(word)
+        
+print(sentence)
+if sentence != "":
+    print("suggestions: ")
+    for word in incorrect_words:
+        
+        suggest_list = difflib.get_close_matches(word,wordlists)
+        print(f'{word}: {", ".join(suggest_list)}')
+    
