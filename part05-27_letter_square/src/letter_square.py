e4893f5a480db562442b0b14d53db59c1eb8f7f6
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-04-23 15:09:48 +0800

    Exercise Completed

--- part05-27_letter_square/src/letter_square.py
+++ part05-27_letter_square/src/letter_square.py
@@ -1,1 +1,15 @@
 # Write your solution here
+# (2n-1) x (2n-1)
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+layer = int(input("Layers: "))
+size = (2 * layer) - 1
+center = layer - 1
+
+for i in range(size):
+    for j in range(size):
+        dist_to_i = abs(i - center)
+        dist_to_j = abs(j - center)
+        max_dist = max(dist_to_i, dist_to_j)
+        print(alphabet[max_dist], end="")
+    print()
