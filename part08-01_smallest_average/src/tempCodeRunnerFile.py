2c8428a299f139ed1771838206d60dd181de680c
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-05-04 10:54:36 +0800

    Added Part 8

---
+++ part08-01_smallest_average/src/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+ "result1": 2, "result2": 3, "result3": 3}
+    person2 = {"name": "Gary", "result1": 5, "result2": 1, "result3": 8}
+    person3 = {"name": "Larry", "result1": 3, "result2": 1, "result3": 1}
+
+    print(smallest_average(per
