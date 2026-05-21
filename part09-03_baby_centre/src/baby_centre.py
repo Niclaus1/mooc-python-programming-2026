9529e230a617a49396ec6587ccbeb3c893cfc4f0
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-05-21 16:29:30 +0800

    Exercise Completed

--- part09-03_baby_centre/src/baby_centre.py
+++ part09-03_baby_centre/src/baby_centre.py
@@ -1,6 +1,6 @@
-
 # WRITE YOUR SOLUTION HERE:
-#Note! Do not change the class Person!
+# Note! Do not change the class Person!
+
 
 class Person:
     def __init__(self, name: str, age: int, height: int, weight: int):
@@ -9,10 +9,39 @@ class Person:
         self.height = height
         self.weight = weight
 
+
 class BabyCentre:
     def __init__(self):
         self.number_of_weigh_ins = 0
 
     def weigh(self, person: Person):
         # return the weight of the person passed as an argument
-        return -1
+        self.number_of_weigh_ins += 1
+        return person.weight
+
+    def feed(self, person: Person):
+        person.weight += 1
+
+    def weigh_ins(self):
+        return self.number_of_weigh_ins
+
+
+if __name__ == "__main__":
+    baby_centre = BabyCentre()
+
+    eric = Person("Eric", 1, 110, 7)
+    peter = Person("Peter", 33, 176, 85)
+
+    print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+
+    print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+
+    print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
