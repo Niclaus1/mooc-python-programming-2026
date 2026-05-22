f176b4a6c4dce282b4382939c991800a0346443a
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-05-22 12:27:45 +0800

    Exercise Completed

--- part09-08_shortest_in_room/src/shortest_in_room.py
+++ part09-08_shortest_in_room/src/shortest_in_room.py
@@ -6,3 +6,65 @@ class Person:
 
     def __str__(self):
         return self.name
+
+
+class Room:
+    def __init__(self):
+        self.room = []
+        self.total_height = 0
+
+    def add(self, person: Person):
+        if person not in self.room:
+            self.room.append(person)
+            self.total_height += person.height
+
+    def is_empty(self):
+        if not self.room:
+            return True
+        else:
+            return False
+
+    def print_contents(self):
+        print(
+            f"There are {len(self.room)} persons in the room, and their combined height is {self.total_height} cm"
+        )
+        for person in self.room:
+            print(f"{person.name} ({person.height})")
+
+    def shortest(self):
+        if self.is_empty():
+            return None
+        else:
+            shortest = self.room[0]
+            for person in self.room:
+                if person.height < shortest.height:
+                    shortest = person
+
+            return shortest
+
+    def remove_shortest(self):
+        if self.is_empty():
+            return None
+        else:
+            person = self.shortest()
+            self.room.remove(person)
+            return person
+
+
+if __name__ == "__main__":
+    room = Room()
+
+    room.add(Person("Lea", 183))
+    room.add(Person("Kenya", 172))
+    room.add(Person("Nina", 162))
+    room.add(Person("Ally", 166))
+    room.print_contents()
+
+    print()
+
+    removed = room.remove_shortest()
+    print(f"Removed from room: {removed.name}")
+
+    print()
+
+    room.print_contents()
