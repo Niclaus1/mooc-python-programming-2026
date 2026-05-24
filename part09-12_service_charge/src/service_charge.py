8885ad55d14f2f1af255cd7bfd9c17489b9aa658
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-05-24 13:29:07 +0800

    Exercise Completed

--- part09-12_service_charge/src/service_charge.py
+++ part09-12_service_charge/src/service_charge.py
@@ -1,3 +1,32 @@
 # WRITE YOUR SOLUTION HERE:
+class BankAccount:
+    def __init__(self, owner: str, account_num: str, balance: float):
+        self.__owner = owner
+        self.__account_num = account_num
+        self.__balance = balance
+
+    def deposit(self, amount: float):
+        self.__balance += amount
+        self.__service_charge()
+
+    def withdraw(self, amount: float):
+        if (self.__balance - amount) < 0:
+            return
+        else:
+            self.__balance -= amount
+            self.__service_charge()
+
+    @property
+    def balance(self):
+        return self.__balance
+
+    def __service_charge(self):
+        self.__balance -= self.balance * 0.01
 
 
+if __name__ == "__main__":
+    account = BankAccount("Randy Riches", "12345-6789", 1000)
+    account.withdraw(100)
+    print(account.balance)
+    account.deposit(100)
+    print(account.balance)
