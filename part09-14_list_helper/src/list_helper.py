efc90c0b1e82466bb413946bd58a1a3f8a8c9836
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-05-28 17:01:03 +0800

    Exercise Completed

--- part09-14_list_helper/src/list_helper.py
+++ part09-14_list_helper/src/list_helper.py
@@ -1,1 +1,28 @@
 # WRITE YOUR SOLUTION HERE:
+class ListHelper:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def greatest_frequency(cls, my_list: list):
+        most_common = my_list[0]
+
+        for item in my_list:
+            if my_list.count(item) > my_list.count(most_common):
+                most_common = item
+        return most_common
+
+    @classmethod
+    def doubles(cls, my_list: list):
+        twice_appeared = []
+
+        for item in my_list:
+            if my_list.count(item) > 1 and item not in twice_appeared:
+                twice_appeared.append(item)
+        return len(twice_appeared)
+
+
+if __name__ == "__main__":
+    numbers = [1, 1, 2, 1, 3, 3, 4, 5, 5, 5, 6, 5, 5, 5]
+    print(ListHelper.greatest_frequency(numbers))
+    print(ListHelper.doubles(numbers))
