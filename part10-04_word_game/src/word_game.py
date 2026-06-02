fd5998f4b62ad5a4db83fc17668a9cf9785d10ae
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-06-02 13:22:31 +0800

    Exercise Completed

--- part10-04_word_game/src/word_game.py
+++ part10-04_word_game/src/word_game.py
@@ -1,7 +1,8 @@
 # Write your solution here
 import random
 
-class WordGame():
+
+class WordGame:
     def __init__(self, rounds: int):
         self.wins1 = 0
         self.wins2 = 0
@@ -13,7 +14,7 @@ class WordGame():
 
     def play(self):
         print("Word game:")
-        for i in range(1, self.rounds+1):
+        for i in range(1, self.rounds + 1):
             print(f"round {i}")
             answer1 = input("player1: ")
             answer2 = input("player2: ")
@@ -25,9 +26,71 @@ class WordGame():
                 self.wins2 += 1
                 print("player 2 won")
             else:
-                pass # it's a tie
+                pass  # it's a tie
 
         print("game over, wins:")
         print(f"player 1: {self.wins1}")
         print(f"player 2: {self.wins2}")
 
+
+class LongestWord(WordGame):
+    def __init__(self, rounds):
+        super().__init__(rounds)
+
+    def round_winner(self, player1_word, player2_word):
+        if len(player1_word) > len(player2_word):
+            return 1
+        elif len(player1_word) < len(player2_word):
+            return 2
+
+
+class MostVowels(WordGame):
+    def __init__(self, rounds):
+        super().__init__(rounds)
+
+    def round_winner(self, player1_word, player2_word):
+        vowels = "aeiouAEIOU"
+        count1 = 0
+        count2 = 0
+
+        for char in player1_word:
+            if char in vowels:
+                count1 += 1
+
+        for char in player2_word:
+            if char in vowels:
+                count2 += 1
+
+        if count1 > count2:
+            return 1
+        elif count1 < count2:
+            return 2
+
+
+class RockPaperScissors(WordGame):
+    def __init__(self, rounds):
+        super().__init__(rounds)
+
+    def round_winner(self, player1_word, player2_word):
+        rules = ["rock", "paper", "scissors"]
+        if player1_word in rules and player2_word not in rules:
+            return 1
+        elif player1_word not in rules and player2_word in rules:
+            return 2
+        elif player1_word not in rules and player2_word not in rules:
+            return 0
+        elif player1_word == "rock" and player2_word == "scissors":
+            return 1
+        elif player1_word == "paper" and player2_word == "rock":
+            return 1
+        elif player1_word == "scissors" and player2_word == "paper":
+            return 1
+        elif player1_word == player2_word:
+            return 0
+        else:
+            return 2
+
+
+if __name__ == "__main__":
+    p = RockPaperScissors(4)
+    p.play()
