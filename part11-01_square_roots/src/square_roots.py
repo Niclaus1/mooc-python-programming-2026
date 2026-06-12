53e095acdcb55f8e100825341512ddeeda26f494
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-06-12 14:01:41 +0800

    Exercise Part 11 Not Completed

--- part11-01_square_roots/src/square_roots.py
+++ part11-01_square_roots/src/square_roots.py
@@ -1,1 +1,9 @@
 # WRITE YOUR SOLUTION HERE:
+def square_roots(numbers: int):
+    return [number**0.5 for number in numbers]
+
+
+if __name__ == "__main__":
+    lines = square_roots([1, 2, 3, 4])
+    for line in lines:
+        print(line)
