92ea0004663c04a7f3d5263e54855205628cdb24
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-06-14 10:36:02 +0800

    Exercise Completed

--- part11-08_filter_forbidden/src/filter_forbidden.py
+++ part11-08_filter_forbidden/src/filter_forbidden.py
@@ -1,1 +1,10 @@
 # WRITE YOUR SOLUTION HERE:
+def filter_forbidden(string: str, forbidden: str):
+    wordList = [word for word in string]
+    return "".join([char for char in wordList if char not in forbidden])
+
+
+if __name__ == "__main__":
+    sentence = "Once! upon, a time: there was a python!??!?!"
+    filtered = filter_forbidden(sentence, "!?:,.")
+    print(filtered)
