78a9be98bd4f3e6b407d0a10eb068a659f1dca99
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-07-02 16:32:08 +0800

    Exercise Completed

--- part12-03_ratings/src/ratings.py
+++ part12-03_ratings/src/ratings.py
@@ -1,1 +1,17 @@
-# TEE RATKAISUSI TÄHÄN:
+def sort_by_ratings(items: list):
+    def sort_order(item: list):
+        return item["rating"]
+
+    return sorted(items, key=sort_order, reverse=True)
+
+
+if __name__ == "__main__":
+    shows = [
+        {"name": "Dexter", "rating": 8.6, "seasons": 9},
+        {"name": "Friends", "rating": 8.9, "seasons": 10},
+        {"name": "Simpsons", "rating": 8.7, "seasons": 32},
+    ]
+
+    print("Rating according to IMDB")
+    for show in sort_by_ratings(shows):
+        print(f"{show['name']}  {show['rating']}")
