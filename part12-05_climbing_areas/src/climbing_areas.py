8fc3e55d6b64fa07cc0b045493b95d17c80043ec
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-07-06 16:41:58 +0800

    Exercise Completed

--- part12-05_climbing_areas/src/climbing_areas.py
+++ part12-05_climbing_areas/src/climbing_areas.py
@@ -7,6 +7,7 @@ class ClimbingRoute:
     def __str__(self):
         return f"{self.name}, length {self.length} metres, grade {self.grade}"
 
+
 class ClimbingArea:
     def __init__(self, name: str):
         self.name = name
@@ -30,3 +31,39 @@ class ClimbingArea:
         hardest_route = self.hardest_route()
         return f"{self.name} {self.routes()} routes, hardest {hardest_route.grade}"
 
+
+def by_length(route: ClimbingArea):
+    return route.routes()
+
+
+def sort_by_number_of_routes(route: list):
+    return sorted(route, key=by_length)
+
+
+def by_difficulty(route: ClimbingArea):
+    return route.hardest_route().grade
+
+
+def sort_by_most_difficult(route: list):
+    return sorted(route, key=by_difficulty, reverse=True)
+
+
+if __name__ == "__main__":
+    ca1 = ClimbingArea("Olhava")
+    ca1.add_route(ClimbingRoute("Edge", 38, "6A+"))
+    ca1.add_route(ClimbingRoute("Great cut", 36, "6B"))
+    ca1.add_route(ClimbingRoute("Swedish route", 42, "5+"))
+
+    ca2 = ClimbingArea("Nummi")
+    ca2.add_route(ClimbingRoute("Synchro", 14, "8C+"))
+
+    ca3 = ClimbingArea("Nalkkila slab")
+    ca3.add_route(ClimbingRoute("Small steps", 12, "6A+"))
+    ca3.add_route(ClimbingRoute("Smooth operator", 11, "7A"))
+    ca3.add_route(ClimbingRoute("Piggy not likey", 12, "6B+"))
+    ca3.add_route(ClimbingRoute("Orchard", 8, "6A"))
+
+    areas = [ca1, ca2, ca3]
+
+    for area in sort_by_most_difficult(areas):
+        print(area)
