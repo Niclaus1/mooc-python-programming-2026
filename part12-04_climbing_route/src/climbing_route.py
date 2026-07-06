6217e9a6fa01734e4768ffa4a7a90a2f44b96b79
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-07-06 16:17:28 +0800

    Exercise Completed

--- part12-04_climbing_route/src/climbing_route.py
+++ part12-04_climbing_route/src/climbing_route.py
@@ -7,4 +7,29 @@ class ClimbingRoute:
     def __str__(self):
         return f"{self.name}, length {self.length} metres, grade {self.grade}"
 
-# Write your solution herer:
+
+def by_length(route: ClimbingRoute):
+    return route.length
+
+
+def by_grade(route: ClimbingRoute):
+    return (route.grade, route.length)
+
+
+# Write your solution here:
+def sort_by_length(routes: list):
+    return sorted(routes, key=by_length, reverse=True)
+
+
+def sort_by_difficulty(routes: list):
+    return sorted(routes, key=by_grade, reverse=True)
+
+
+if __name__ == "__main__":
+    r1 = ClimbingRoute("Edge", 38, "6A+")
+    r2 = ClimbingRoute("Smooth operator", 9, "7A")
+    r3 = ClimbingRoute("Syncro", 14, "8C+")
+    reply = sort_by_difficulty([r1, r2, r3])
+
+    for route in reply:
+        print(route)
