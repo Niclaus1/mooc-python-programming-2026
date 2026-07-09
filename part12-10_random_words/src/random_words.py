94590abcf4ac5c581a46f577b68a75e2b497050d
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-07-09 16:45:38 +0800

    Exercise Completed

--- part12-10_random_words/src/random_words.py
+++ part12-10_random_words/src/random_words.py
@@ -1,1 +1,13 @@
+import random
+
+
 # Write your solution here:
+def word_generator(characters: str, length: int, amount: int):
+    for _ in range(amount):
+        yield "".join(random.sample(characters, k=length))
+
+
+if __name__ == "__main__":
+    wordgen = word_generator("abcdefg", 3, 5)
+    for word in wordgen:
+        print(word)
