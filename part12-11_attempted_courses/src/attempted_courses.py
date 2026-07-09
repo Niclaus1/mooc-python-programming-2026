7f587ab31a6ea0e2b0d4b6dfffcde030af3e7a51
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-07-09 17:03:31 +0800

    Exercise Completed

--- part12-11_attempted_courses/src/attempted_courses.py
+++ part12-11_attempted_courses/src/attempted_courses.py
@@ -5,6 +5,24 @@ class CourseAttempt:
         self.grade = grade
 
     def __str__(self):
-        return f"{self.student_name}, grade for the course {self.course_name} {self.grade}"
+        return (
+            f"{self.student_name}, grade for the course {self.course_name} {self.grade}"
+        )
+
+
+def names_of_students(attempts: list):
+    return map(lambda x: x.student_name, attempts)
+
+
+def course_names(attempts: list):
+    return list(set(map(lambda x: x.course_name, attempts)))
+
 
 # Write your solution here
+if __name__ == "__main__":
+    s1 = CourseAttempt("Peter Python", "Introduction to Programming", 3)
+    s2 = CourseAttempt("Olivia C. Objective", "Introduction to Programming", 5)
+    s3 = CourseAttempt("Peter Python", "Advanced Course in Programming", 2)
+
+    for name in course_names([s1, s2, s3]):
+        print(name)
