701d23403dfe68008e8c1a0bbbb586971202dc7b
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-07-09 16:17:15 +0800

    Exercise Completed

--- part12-09_prime_numbers/src/prime_numbers.py
+++ part12-09_prime_numbers/src/prime_numbers.py
@@ -1,1 +1,20 @@
 # Write your solution here
+def prime_numbers():
+
+    number = 2
+    while True:
+        is_prime = True
+        for num in range(2, int(number**0.5) + 1):
+            if number % num == 0:
+                is_prime = False
+                break
+
+        if is_prime:
+            yield number
+        number += 1
+
+
+if __name__ == "__main__":
+    numbers = prime_numbers()
+    for i in range(8):
+        print(next(numbers))
