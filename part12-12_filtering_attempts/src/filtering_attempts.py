6d136983c8b30a9caf6f80bbc10ec3ce2fa6f0cd
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-07-09 19:33:48 +0800

    Exercise Completed

--- part12-12_filtering_attempts/src/filtering_attempts.py
+++ part12-12_filtering_attempts/src/filtering_attempts.py
@@ -5,6 +5,36 @@ class CourseAttempt:
         self.grade = grade
 
     def __str__(self):
-        return f"{self.student_name}, grade for the course {self.course_name} {self.grade}"
+        return (
+            f"{self.student_name}, grade for the course {self.course_name} {self.grade}"
+        )
 
 
+def accepted(attempts: list):
+    return filter(lambda student: student.grade > 0, attempts)
+
+
+def attempts_with_grade(attempts: list, grade: int):
+    return filter(lambda student: student.grade == grade, attempts)
+
+
+def passed_students(attempts: list, course: str):
+    return sorted(
+        map(
+            lambda student: student.student_name,
+            filter(
+                lambda student: student.course_name == course and student.grade > 0,
+                attempts,
+            ),
+        )
+    )
+
+
+if __name__ == "__main__":
+    s1 = CourseAttempt("Peter Python", "Introduction to Programming", 3)
+    s2 = CourseAttempt("Olivia C. Objective", "Introduction to AI", 5)
+    s3 = CourseAttempt("Peter Python", "Introduction to AI", 0)
+    s4 = CourseAttempt("Jack Java", "Introduction to AI", 3)
+
+    for attempt in passed_students([s1, s2, s3, s4], "Introduction to AI"):
+        print(attempt)
