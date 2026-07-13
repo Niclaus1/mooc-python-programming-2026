06754edeb4cfb61714b593beece7d04f5c6f0417
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-07-13 22:58:34 +0800

    Exercise Completed

--- part13-01_four_robots/src/main.py
+++ part13-01_four_robots/src/main.py
@@ -1,4 +1,3 @@
-
 # The exercises in this part of the course have no automated tests, as the results as visually verified.
 # The tests grant points automatically as you submit your solution to the server, no matter what your implementation.
 # Only submit your solution when you are ready, and your solution matches the exercise description.
@@ -6,4 +5,29 @@
 # If your solution clearly does not match the exercise description, you may lose the points granted for the exercises in this part.
 
 # WRITE YOUR SOLUTION HERE:
+import pygame
+from PIL import Image
 
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+image = Image.open("robot.png")
+image.save("output.bmp")
+robot = pygame.image.load("output.bmp")
+width = robot.get_width()
+height = robot.get_height()
+
+window.fill((0, 0, 0))
+# window.blit(robot, (0, 0))
+
+window.blit(robot, (0, 0))
+window.blit(robot, (640 - width, 0))
+window.blit(robot, (0, 480 - height))
+window.blit(robot, (640 - width, 480 - height))
+
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
