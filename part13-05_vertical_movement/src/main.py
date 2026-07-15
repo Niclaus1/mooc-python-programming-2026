ce076801823362a96745c457836662158d0ef1d3
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-07-15 19:13:09 +0800

    Exercise Completed

--- part13-05_vertical_movement/src/main.py
+++ part13-05_vertical_movement/src/main.py
@@ -1,1 +1,31 @@
-# WRITE YOUR SOLUTION HERE:
+import pygame
+from PIL import Image
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+image = Image.open("robot.png")
+image.save("output.bmp")
+robot = pygame.image.load("output.bmp")
+
+x = 0
+y = 0
+velocity = 1
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    window.fill((0, 0, 0))
+    window.blit(robot, (x, y))
+    pygame.display.flip()
+
+    y += velocity
+    if velocity > 0 and y + robot.get_height() >= 480:
+        velocity = -velocity
+    if velocity < 0 and y <= 0:
+        velocity = -velocity
+
+    clock.tick(60)
