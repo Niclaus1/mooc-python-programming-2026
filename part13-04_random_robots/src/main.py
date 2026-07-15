52df904875daea4701b4f5156b7fb08c2c7cd8d2
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-07-15 18:45:58 +0800

    Exercise Completed

--- part13-04_random_robots/src/main.py
+++ part13-04_random_robots/src/main.py
@@ -1,1 +1,29 @@
 # WRITE YOUR SOLUTION HERE:
+# WRITE YOUR SOLUTION HERE:
+import random
+
+import pygame
+from PIL import Image
+
+pygame.init()
+window = pygame.display.set_mode((800, 480))
+
+image = Image.open("robot.png")
+image.save("output.bmp")
+robot = pygame.image.load("output.bmp")
+
+width = robot.get_width()
+height = robot.get_height()
+
+window.fill((0, 0, 0))
+for row in range(1000):
+    x = random.randint(1, 1000)
+    y = random.randint(1, 1000)
+    window.blit(robot, (x, y))
+
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
