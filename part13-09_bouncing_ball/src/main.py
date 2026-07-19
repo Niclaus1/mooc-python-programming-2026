9b21197c12474ba4529ed2d84384e569bd978499
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-07-19 11:25:27 +0800

    Exercise Completed

--- part13-09_bouncing_ball/src/main.py
+++ part13-09_bouncing_ball/src/main.py
@@ -1,1 +1,40 @@
 # WRITE YOUR SOLUTION HERE:
+import pygame
+from PIL import Image
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+image = Image.open("ball.png")
+image.save("output.bmp")
+robot = pygame.image.load("output.bmp")
+
+x = 0
+y = 0
+xVelocity = 1
+yVelocity = 1
+clock = pygame.time.Clock()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    window.fill((0, 0, 0))
+    window.blit(robot, (x, y))
+    pygame.display.flip()
+
+    y += yVelocity
+    if yVelocity > 0 and y + robot.get_height() >= 480:
+        yVelocity = -yVelocity
+    if yVelocity < 0 and y <= 0:
+        yVelocity = -yVelocity
+
+    x += xVelocity
+    if xVelocity > 0 and x + robot.get_width() >= 640:
+        xVelocity = -xVelocity
+    if yVelocity < 0 and y <= 0:
+        xVelocity = -xVelocity
+
+    clock.tick(60)
