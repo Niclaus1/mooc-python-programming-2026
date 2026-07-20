f6a4e50b8101efaa33b2bff3c3fb838ffcd1648c
Niclaus G. Rendon <niclausg.rendon@Niclauss-MacBook-Air.local>
2026-07-20 16:25:18 +0800

    Exercise Completed

--- part13-12_four_walls/src/main.py
+++ part13-12_four_walls/src/main.py
@@ -1,1 +1,63 @@
 # WRITE YOUR SOLUTION HERE:
+import pygame
+from PIL import Image
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+image = Image.open("robot.png")
+image.save("output.bmp")
+robot = pygame.image.load("output.bmp")
+x = 0
+y = 480 - robot.get_height()
+
+to_right = False
+to_left = False
+to_up = False
+to_down = False
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                to_left = True
+            if event.key == pygame.K_RIGHT:
+                to_right = True
+            if event.key == pygame.K_DOWN:
+                to_down = True
+            if event.key == pygame.K_UP:
+                to_up = True
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                to_left = False
+            if event.key == pygame.K_RIGHT:
+                to_right = False
+            if event.key == pygame.K_DOWN:
+                to_down = False
+            if event.key == pygame.K_UP:
+                to_up = False
+
+        if event.type == pygame.QUIT:
+            exit()
+
+    if to_right:
+        if not (x + robot.get_width() >= 640):
+            x += 2
+    if to_left:
+        if not (x <= 0):
+            x -= 2
+    if to_down:
+        if not (y + robot.get_height() >= 480):
+            y += 2
+    if to_up:
+        if not (y <= 0):
+            y -= 2
+
+    window.fill((0, 0, 0))
+    window.blit(robot, (x, y))
+    pygame.display.flip()
+
+    clock.tick(60)
